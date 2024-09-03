55e3685e685be8e9811b5222a2dc98b4d992ce1e
Alex <146798485+Alex-97es@users.noreply.github.com>
2024-09-03 22:41:04 +0700

    Fix

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,16 +1,10 @@
 import { combineReducers } from 'redux';
 import {movieReducer} from "./movieReducers";
-import {userReducer} from "./userReducers";
 import authReducer from "./authReducer";
-import {videoReducer} from "./videoReducers";
-import {imageReducer} from "./imageReducers";
 
 const rootReducer = combineReducers({
   movie: movieReducer,
-  user: userReducer,
   auth: authReducer,
-  video: videoReducer,
-  image: imageReducer,
 });
 
 export default rootReducer;
--- src/store/sagas/index.ts
+++ src/store/sagas/index.ts
@@ -1,14 +1,10 @@
 import { all } from 'redux-saga/effects';
-import videoSaga from './movieSagas';
 import userSaga from "./userSagas";
 import movieSaga from "./movieSagas";
-import imageSaga from "./imageSagas";
 
 export default function* rootSaga() {
   yield all([
     movieSaga(),
     userSaga(),
-    videoSaga(),
-    imageSaga(),
   ]);
 }
