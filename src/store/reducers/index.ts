d759accd72e20c235fceb7561f83f302443a0200
Alex <146798485+Alex-97es@users.noreply.github.com>
2024-09-03 22:52:22 +0700

    Fix

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import {movieReducer} from "./movieReducers";
 import authReducer from "./authReducer";
+import {userReducer} from "./userReducers";
 
 const rootReducer = combineReducers({
   movie: movieReducer,
+  user: userReducer,
   auth: authReducer,
 });
 
